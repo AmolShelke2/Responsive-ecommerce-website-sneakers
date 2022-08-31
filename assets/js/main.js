2bd20367ff9270e052af56d719b958c3f4ac6aa7
Amol shelke <shelkeamol897@gmail.com>
2022-08-31 12:48:41 +0530

    Updated Header Section with styles and show and hide menu feature

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,59 @@
-/*===== MENU SHOW =====*/ 
+/*===== MENU SHOW =====*/
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId);
+  nav = document.getElementById(navId);
 
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show");
+    });
+  }
+};
+
+showMenu("nav-toggle", "nav-menu");
 
 /*===== REMOVE MENU =====*/
+const navLink = document.querySelectorAll(".nav__link");
+navMenu = document.getElementById("nav-menu");
 
+function linkAction() {
+  navMenu.classList.remove("show");
+}
+
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*===== SCROLL SECTIONS ACTIVE LINK =====*/
+const sections = document.querySelectorAll(`section[id]`);
 
+window.addEventListener("scroll", scrollActive);
 
-/*===== CHANGE COLOR HEADER =====*/ 
+function scrollActive() {
+  const scrollV = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active");
+
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("remove");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active");
+    }
+  });
+}
+
+/*===== CHANGE COLOR HEADER =====*/
+window.onscroll = () => {
+  constnav = document.getElementById("header");
+  if (this.scrollY >= 200) nav.classList.add("scroll-header");
+  else nav.classList.remove("scroll-header");
+};
